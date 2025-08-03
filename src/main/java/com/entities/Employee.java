e2577246dfbde40c2c9ebd56f0a352ce22e34583
Neeraj <pandeyneerajmala@gmail.com>
2025-08-03 16:39:56 +0530

    Add Employee and Laptop entities with DAO methods for database operations.

---
+++ src/main/java/com/entities/Employee.java
@@ -0,0 +1,61 @@
+package com.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+import java.util.List;
+
+@Entity
+public class Employee {
+
+    @Id
+    private int empId;
+    private String empName;
+    private String tech;
+    @OneToMany(mappedBy = "employee") // Assuming Laptop has a field named 'employee'
+    private List<Laptop> laptop;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public List<Laptop> getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(List<Laptop> laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", tech='" + tech + '\'' +
+                ", laptop=" + laptop +
+                '}';
+    }
+}
